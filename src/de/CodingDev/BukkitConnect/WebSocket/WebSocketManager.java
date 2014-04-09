d8ac0551a07c176a596c071b5bbf4f19e8e72b49
R3N3PDE <rene.preuss@hostinfin.net>
2014-04-09 21:27:10 +0200

    Add WebSocketManager

---
+++ src/de/CodingDev/BukkitConnect/WebSocket/WebSocketManager.java
@@ -0,0 +1,15 @@
+package de.CodingDev.BukkitConnect.WebSocket;
+
+import de.CodingDev.BukkitConnect.BukkitConnect;
+
+public class WebSocketManager {
+	private BukkitConnect bukkitConnect;
+	
+	public WebSocketManager(BukkitConnect bukkitConnect){
+		this.bukkitConnect = bukkitConnect;
+	}
+	
+	public void regiserChannel(){
+		
+	}
+}
